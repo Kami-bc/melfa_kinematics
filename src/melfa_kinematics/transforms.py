7fd7888a15c9ea4a0e8a64e0ec1d2a7bfa5a3c34
Kami <karol.benavidesc@uniagustiniana.edu.co>
2026-05-27 04:52:02 -0500

    Add virtual environment setup and rotation transforms

---
+++ src/melfa_kinematics/transforms.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def Rx(theta):
+    c = np.cos(theta)
+    s = np.sin(theta)
+
+    return np.array([
+        [1, 0, 0],
+        [0, c, -s],
+        [0, s, c]
+    ])
+
+
+def Ry(theta):
+    c = np.cos(theta)
+    s = np.sin(theta)
+
+    return np.array([
+        [c, 0, s],
+        [0, 1, 0],
+        [-s, 0, c]
+    ])
+
+
+def Rz(theta):
+    c = np.cos(theta)
+    s = np.sin(theta)
+
+    return np.array([
+        [c, -s, 0],
+        [s, c, 0],
+        [0, 0, 1]
+    ])
